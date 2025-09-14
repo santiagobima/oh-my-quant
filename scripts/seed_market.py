da0cfca4058a76bf36aec8f9009358e8bc76ddcf
santiago bima <santiagobima@santiagos-MacBook-Pro.local>
2025-09-15 01:26:41 +0200

    cambios efectuados

--- scripts/seed_market.py
+++ scripts/seed_market.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+
+# Descargamos la tabla de Wikipedia que contiene las empresas del S&P 500
  
 URL = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
@@ -7,9 +9,9 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 }
 
-
 response = requests.get(URL, headers=headers)
 tables = pd.read_html(response.text)
-
 sp500_table = tables[0]
-print(sp500_table.head())
+df = sp500_table[['Symbol','Security']].copy()
+df['kind'] = 'equity'
+print(df.head())
